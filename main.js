be65ef34ae8c8627de009c46ab256a601103b1f2
Brian <briandsalemi@gmail.com>
2019-08-23 12:48:36 -0400

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const hearts = document.querySelectorAll('.like')
+console.log(hearts)
+console.log(FULL_HEART)
 // Your JavaScript code goes here!
 
-
+hearts.forEach(heart => {
+    heart.addEventListener('click', function(e) {
+    if(e.target.innerHTML === EMPTY_HEART){
+      mimicServerCall()
+        .then((resp) => {
+          let heartEmoji = heart.querySelector('.like-glyph');
+          heartEmoji.innerHTML = FULL_HEART;
+          heartEmoji.className = "activated-heart";
+        })
+        .catch(() => {
+         let hiddenDiv = document.getElementById("modal")
+         hiddenDiv.className = ""
+         setTimeout(function(){hiddenDiv.classname = "hidden"}, 5000)
+        })
+    } else if (e.target.innerHTML === FULL_HEART){
+      let heartEmoji = heart.querySelector('.activated-heart');
+          heartEmoji.innerHTML = EMPTY_HEART;
+          heartEmoji.className = "like-glyph";
+    }
+  }
+);
+})
 
 
 //------------------------------------------------------------------------------
